c092bda753f22e7decf9b58f81d03ef45bb614b7
yaelglz <yaelglzmunoz@gmail.com>
2023-02-09 17:33:55 -0600

    Datos y Variables - Practica JS

---
+++ C8-INTRO-WEB-7/js/domJs.js
@@ -0,0 +1,4 @@
+function evento() {
+    document.write("Hola esta es introducción a JS con funciones y DOM");
+    alert("Lokk at me, I'm an alert box!")
+}
---
+++ C8-JS-01/js/tipos-de-datos-y-variables.js
@@ -0,0 +1,13 @@
+// Variable tipo var uso 
+var nombre = "Yael"; //string
+var edad = 25; //number
+var tengoMascota = true; //boolean
+var tengoTrabajo = false; //boolean
+var tengoSueldo; //undefined
+
+// Variable tipo let uso
+let nombre = "Yael"; //string
+let edad = 25; //number
+let tengoMascota = true; //boolean
+let tengoTrabajo = false; //boolean
+let tengoSueldo; //undefined
